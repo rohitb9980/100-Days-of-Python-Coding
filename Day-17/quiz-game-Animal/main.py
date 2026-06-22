817d484ecc505ade78dd08d654323baf10996d82
rohitb9980 <rohitb9980@gmail.com>
2026-06-22 23:02:58 +0530

    Day 17

---
+++ Day-17/quiz-game-Animal/main.py
@@ -0,0 +1,18 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import Quizbrain
+
+question_bank = []
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+quiz = Quizbrain(question_bank)
+
+while quiz.still_has_question():
+    quiz.next_question()
+
+print("You have finished the quiz")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")

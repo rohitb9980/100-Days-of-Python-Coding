42ed7bd55dc6daad0cbd7593426a57320038d5ec
rohitb9980 <rohitb9980@gmail.com>
2026-07-12 09:37:56 +0530

    new

--- Day-23/main.py
+++ Day-23/main.py
@@ -1,39 +1,51 @@
-import time
 from turtle import Screen
-from player import Player
-from car_manager import CarManager
+from snake import Snake
+from food import Food
 from scoreboard import Scoreboard
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
 screen.tracer(0)
 
-player = Player()
-car_manager = CarManager()
+snake = Snake()
+food = Food()
 scoreboard = Scoreboard()
 
 screen.listen()
-screen.onkey(player.go_up, "Up")
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
     screen.update()
+    time.sleep(0.1)
+    snake.move()
 
-    car_manager.create_car()
-    car_manager.move_cars()
+    #Detect collision with food.
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
 
-    #Detect collision with car
-    for car in car_manager.all_cars:
-        if car.distance(player) < 20:
-            game_is_on = False
-            scoreboard.game_over()
+    #Detect collision with wall.
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
 
-    #Detect successful crossing
-    if player.is_at_finish_line():
-        player.go_to_start()
-        car_manager.level_up()
-        scoreboard.increase_level()
+    #Detect collision with tail.
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            scoreboard.reset()
+
+
+
 
 
 screen.exitonclick()
--- Day-23/scoreboard.py
+++ Day-23/scoreboard.py
@@ -1,5 +1,5 @@
 from turtle import Turtle
-
+ALIGNMENT = "center"
 FONT = ("Courier", 24, "normal")
 
 
@@ -7,20 +7,29 @@ class Scoreboard(Turtle):
 
     def __init__(self):
         super().__init__()
-        self.level = 1
-        self.hideturtle()
+        self.score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
+        self.color("white")
         self.penup()
-        self.goto(-280, 250)
+        self.goto(0, 270)
+        self.hideturtle()
         self.update_scoreboard()
 
     def update_scoreboard(self):
         self.clear()
-        self.write(f"Level: {self.level}", align="left", font=FONT)
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
-    def increase_level(self):
-        self.level += 1
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
+        self.score = 0
         self.update_scoreboard()
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write(f"GAME OVER", align="center", font=FONT)
+    def increase_score(self):
+        self.score += 1
+        self.update_scoreboard()
+
+
